f685de9830eec86121d411088216facda58cda2f
ankit <ankitkejriwaal@gmail.com>
2020-02-09 17:45:02 -0500

    examples of string type

--- src/ArraySet1.java
+++ src/ArraySet1.java
@@ -88,6 +88,62 @@ public class ArraySet1 {
         }
     }
 
+    private static int fibonacci(int n){
+        int num1 = 0;
+        int num2 = 1;
+        int res =0;
+        if( n == 1)
+            return num1;
+        if(n == 2)
+            return num2;
+        for(int i =3;i<=n+1;i++){
+            res = num1 + num2;
+            num1 = num2;
+            num2 = res;
+        }
+        return res;
+    }
+
+    private static int maxSubArraySum(int[] arr){
+        int currsum =0;
+        int maxsum = 0;
+        for(int i=0;i<arr.length;i++){
+            currsum+= arr[i];
+            if(maxsum<currsum)
+                maxsum = currsum;
+            if(currsum < 0)
+                currsum =0;
+        }
+        return maxsum;
+    }
+
+    private static int maximumAvg(int[] arr,int k){
+        int maxsum = 0,sum =0;
+        for(int i=0;i<arr.length -k ;i++){
+            sum =0;
+            for(int j=i;j<i+k;j++){
+                sum+= arr[j];
+            }
+            if(sum> maxsum)
+                maxsum = sum;
+        }
+        return maxsum;
+    }
+
+    private static int efficientMaxSum(int[] arr, int k) {
+        int maxsum = 0, sum =0;
+        for(int i=0;i<k;i++){
+            sum+= arr[i];
+        }
+        maxsum = sum;
+        for(int i =k;i<arr.length;i++){
+            sum = sum+ arr[i]- arr[i-k];
+            if(sum>maxsum)
+                maxsum = sum;
+        }
+        return maxsum;
+    }
+
     public static void main(String[] args) {
         int[] arr = {10,1,20,5,7,0,15,8};
         selectionSort(arr);
@@ -98,5 +154,11 @@ public class ArraySet1 {
         int[] arr2 = {1,2,3,4,5};
         efficientRotateArray(arr2,6);
         printArr(arr2);
+        System.out.println(fibonacci(10));
+        int[] arr3 = {2, -9, 5, 1, -4, 6, 0, -7, 8};
+        System.out.println(maxSubArraySum(arr3));
+        int[] arr4 = {11, -8, 16, -7, 24, -2, 3};
+        System.out.println(maximumAvg(arr4,3));
+        System.out.println(efficientMaxSum(arr4,3));
     }
 }
